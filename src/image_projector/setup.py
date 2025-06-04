2d655667458f4b031421784d3922f2d8c928d78e
ZongZheJiang <jiangzongzhe374@gmail.com>
2025-06-04 17:34:52 +0800

    initial commit

---
+++ src/image_projector/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'image_projector'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools','robopoint_interfaces'],
+    zip_safe=True,
+    maintainer='ddq2',
+    maintainer_email='hominhan56@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            # 'executable_name_in_ros = package_name.python_module_name:main_function'
+            'image_projector = image_projector.image_projector:main',
+            'realsense_projector = image_projector.realsense_projector:main'
+        ],
+    },
+)
